0e83ae00ec50772cd1092847452b6e1ef59a93df
Andrei <93673737+andreidfr0@users.noreply.github.com>
2022-04-18 13:34:45 +0300

    finished

--- src/main/java/com/geekbrains/Car.java
+++ src/main/java/com/geekbrains/Car.java
@@ -1,36 +1,68 @@
 package com.geekbrains;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
+
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
+    static final CountDownLatch START = new CountDownLatch(5);
+    static final CountDownLatch STOP = new CountDownLatch(5);
     private Race race;
     private int speed;
     private String name;
+    private int carNumber;
+    private static boolean winner;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
+
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
-        this.name = "Участник #" + CARS_COUNT;
+        this.carNumber = CARS_COUNT;
+        this.name = "Участник #" + carNumber;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
-        } catch (Exception e) {
+            for (int i = 0; i < race.getStages().size(); i++) {
+                if (i == 0) {
+                    START.countDown();
+                    START.await();
+                }
+                race.getStages().get(i).go(this);
+            }
+            isWin(this); // find finished
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+        } finally {
+            STOP.countDown();
+            try {
+                STOP.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    private static synchronized void isWin(Car c) {
+        if (!winner) {
+            System.out.println(c.name + " - FINISHED");
+            winner = true;
+        }
+    }
+
 }
--- src/main/java/com/geekbrains/Tunnel.java
+++ src/main/java/com/geekbrains/Tunnel.java
@@ -1,20 +1,41 @@
 package com.geekbrains;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+    private static final int CAR_QUEUE = MainClass.CARS_COUNTS / 2;  // По тоннелю могут одновременно ехать только двое
+    private static final boolean[] GATE_PLACES = new boolean[CAR_QUEUE];
+    private static final Semaphore SEMAPHORE = new Semaphore(CAR_QUEUE, false);
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
+
     @Override
     public void go(Car c) {
         try {
             try {
-                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                System.out.println(c.getName() + " начал этап: " + description);
-                Thread.sleep(length / c.getSpeed() * 1000);
+                SEMAPHORE.acquire();
+                int gateNumber = -1;
+                synchronized (GATE_PLACES) {  //Ищем свободное место
+                    for (int j = 0; j < CAR_QUEUE; j++)
+                        if (!GATE_PLACES[j]) {      //Если место свободно
+                            GATE_PLACES[j] = true;  //занимаем его
+                            gateNumber = j;         //Наличие свободного места, гарантирует семафор
+                            System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                            break;
+                        }
+                }
+                synchronized (GATE_PLACES) {
+                    GATE_PLACES[gateNumber] = false;//Освобождаем место
+                    System.out.println(c.getName() + " начал этап: " + description);
+                    Thread.sleep(length / c.getSpeed() * 1000);
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
+                SEMAPHORE.release();
                 System.out.println(c.getName() + " закончил этап: " + description);
             }
         } catch (Exception e) {
